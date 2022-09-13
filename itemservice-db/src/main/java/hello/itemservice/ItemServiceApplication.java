fcbab133e9dd4d2fcfc86fe0b9aad156d68d857b
Jihun Park <jihunpark.tech@gmail.com>
2022-09-13 18:28:17 +0900

    init: project structure

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -8,7 +8,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
-
+/**
+ * @Import(MemoryConfig.class)
+ * - MemoryConfig를 설정 파일로 사용한다
+ *
+ * @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
+ * - 특정 패키지 경로만 컴포넌트 스캔을 할 경우 경로 설정
+ */
 @Import(MemoryConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
@@ -17,6 +23,12 @@ public class ItemServiceApplication {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
+	/**
+	 * @Profile("local")
+	 * - 특정 프로필의 경우에만 해당 스프링 빈을 등록
+	 * - /src/main/resources/application.properties -> spring.profiles.active
+	 * - TestCase 실행 시에는 /src/test/resources/application.properties 파일을 우선으로 참조
+	 */
 	@Bean
 	@Profile("local")
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
--- itemservice-db/src/main/java/hello/itemservice/TestDataInit.java
+++ itemservice-db/src/main/java/hello/itemservice/TestDataInit.java
@@ -14,7 +14,14 @@ public class TestDataInit {
     private final ItemRepository itemRepository;
 
     /**
-     * 확인용 초기 데이터 추가
+     * 확인용 초기 데이터 추가 (메모리 활용 시)
+     * 애플리케이션 실행 시 초기 데이터 저장
+     *
+     * @EventListener(ApplicationReadyEvent.class)
+     * - AOP를 포함한 스프링 컨테이너가 초기화를 완전히 끝내고, 실행 준비가 되었을 때 발생하는 이벤트
+     *
+     * @PostConstruct
+     * - AOP(ex. @Transactional 관련)가 처리되지 않은 시점에 호출될 수 있기 때문에, 간혹 문제 발생
      */
     @EventListener(ApplicationReadyEvent.class)
     public void initData() {
