f8d293fd5d6c099d2884f41ae6b4546bc1522e83
Jihun Park <jihunpark.tech@gmail.com>
2022-09-23 17:39:24 +0900

    [MyBatis] 적용

---
+++ itemservice-db/src/main/java/hello/itemservice/repository/mybatis/ItemMapper.java
@@ -0,0 +1,28 @@
+package hello.itemservice.repository.mybatis;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * MyBatis Mapping XML을 호출해 주는 Mapper Interface
+ * 메서드를 호출하면 XML의 해당 SQL을 실행하고 결과를 반환
+ *
+ * @Mapper MyBatis 에 Mapper로 등록
+ */
+@Mapper
+public interface ItemMapper {
+
+    void save(Item item);
+
+    void update(@Param("id") Long id, @Param("updateParam") ItemUpdateDto updateParam);
+
+    Optional<Item> findById(Long id);
+
+    List<Item> findAll(ItemSearchCond itemSearch);
+}
